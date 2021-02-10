06ae57af6f54ac830d682138f2e9e644bc4986c6
Aleksandr Illarionov <illarionov1997@gmail.com>
2021-02-10 14:19:04 +0200

    Moved "forward" and supporting functions to common

--- source/validation.jl
+++ source/validation.jl
@@ -75,97 +75,6 @@ function reset_validation_data(validation_plot_data::Validation_plot_data,
     return nothing
 end
 
-#---Analysing images in slices
-function prepare_data(input_data::Union{Array{Float32,4},CuArray{Float32,4}},ind_max::Int64,
-        max_value::Int64,offset::Int64,ind_split::Int64,j::Int64)
-    start_ind = 1 + (j-1)*ind_split-1
-    end_ind = min(start_ind + ind_split-1,max_value)
-    correct_size = end_ind-start_ind+1
-    start_ind = start_ind - offset
-    end_ind = end_ind + offset
-    start_ind = start_ind<1 ? 1 : start_ind
-    end_ind = end_ind>max_value ? max_value : end_ind
-    temp_data = input_data[:,start_ind:end_ind,:,:]
-    max_dim_size = size(temp_data,ind_max)
-    offset_add = Int64(ceil(max_dim_size/16)*16) - max_dim_size
-    temp_data = pad(temp_data,[0,offset_add],same)
-    output_data = (temp_data,correct_size,offset_add)
-    return output_data
-end
-
-# Makes output mask to have a correct size for stiching
-function fix_size(temp_predicted::Union{Array{Float32,4},CuArray{Float32,4}},
-        num_parts::Int64,correct_size::Int64,ind_max::Int64,
-        offset_add::Int64,j::Int64)
-    temp_size = size(temp_predicted,ind_max)
-    offset_temp = (temp_size - correct_size) - offset_add
-    if offset_temp>0
-        div_result = offset_add/2
-        offset_add1 = Int64(floor(div_result))
-        offset_add2 = Int64(ceil(div_result))
-        if j==1
-            temp_predicted = temp_predicted[:,
-                (1+offset_add1):(end-offset_temp-offset_add2),:,:]
-        elseif j==num_parts
-            temp_predicted = temp_predicted[:,
-                (1+offset_temp+offset_add1):(end-offset_add2),:,:]
-        else
-            temp = (temp_size - correct_size - offset_add)/2
-            offset_temp = Int64(floor(temp))
-            offset_temp2 = Int64(ceil(temp))
-            temp_predicted = temp_predicted[:,
-                (1+offset_temp+offset_add1):(end-offset_temp2-offset_add2),:,:]
-        end
-    elseif offset_temp<0
-        throw(DomainError("offset_temp should be greater or equal to zero"))
-    end
-end
-
-function accum_parts(model::Chain,input_data::Array{Float32,4},
-        num_parts::Int64,offset::Int64)
-    input_size = size(input_data)
-    max_value = maximum(input_size)
-    ind_max = findfirst(max_value.==input_size)
-    ind_split = convert(Int64,floor(max_value/num_parts))
-    predicted = Vector{Array{Float32,4}}(undef,0)
-    for j = 1:num_parts
-        if j==num_parts
-            ind_split = ind_split+rem(max_value,num_parts)
-        end
-        temp_data,correct_size,offset_add =
-            prepare_data(input_data,ind_max,max_value,offset,ind_split,j)
-        temp_predicted = model(temp_data)
-        temp_predicted =
-            fix_size(temp_predicted,num_parts,correct_size,ind_max,offset_add,j)
-        push!(predicted,temp_predicted)
-    end
-    predicted_out = reduce(vcat,predicted)
-    return predicted_out
-end
-
-function accum_parts(model::Chain,input_data::CuArray{Float32,4},
-        num_parts::Int64,offset::Int64)
-    input_size = size(input_data)
-    max_value = maximum(input_size)
-    ind_max = findfirst(max_value.==input_size)
-    ind_split = convert(Int64,floor(max_value/num_parts))
-    predicted = Vector{CuArray{Float32,4}}(undef,0)
-    for j = 1:num_parts
-        if j==num_parts
-            ind_split = ind_split+rem(max_value,num_parts)
-        end
-        temp_data,correct_size,offset_add =
-            prepare_data(input_data,ind_max,max_value,offset,ind_split,j)
-        temp_predicted::CuArray{Float32,4} = model(temp_data)
-        temp_predicted =
-            fix_size(temp_predicted,num_parts,correct_size,ind_max,offset_add,j)
-        push!(predicted,collect(temp_predicted))
-        CUDA.unsafe_free!(temp_predicted)
-    end
-    predicted_out::CuArray{Float32,4} = hcat(predicted...)
-    return predicted_out
-end
-
 #---Makes output images
 function do_target!(target_temp::Vector{Array{RGB{Float32},2}},
         target::Array{Float32,2},color::Array{Float32,3},j::Int64)
@@ -284,28 +193,6 @@ function output_and_error_images(predicted_array::Vector{BitArray{3}},
         @everywhere GC.safepoint()
     end
     return predicted_color,predicted_error,target_color
-end
-
-#---Main application funtions
-# Runs data thorugh a neural network
-function forward(model::Chain,input_data::Array{Float32};
-        num_parts::Int64=1,offset::Int64=0,use_GPU::Bool=true)
-    if use_GPU
-        input_data_gpu = CuArray(input_data)
-        model = move(model,gpu)
-        if num_parts==1
-            predicted = collect(model(input_data_gpu))
-        else
-            predicted = collect(accum_parts(model,input_data_gpu,num_parts,offset))
-        end
-    else
-        if num_parts==1
-            predicted = model(input_data)
-        else
-            predicted = accum_parts(model,input_data,num_parts,offset)
-        end
-    end
-    return predicted::Array{Float32,4}
 end
 
 # Main validation function
